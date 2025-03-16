adadcb7b72aca7433ea028f68bea5bf8701b7658
AlexChanshuo <alexma@goldenraintree.tw>
2025-03-16 20:41:17 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import FinalBusinessReport from './FinalBusinessReport.tsx'
+// @ts-ignore
+import FinalBusinessReport from './FinalBusinessReport.jsx';
 import './index.css' // 如果有的話
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
